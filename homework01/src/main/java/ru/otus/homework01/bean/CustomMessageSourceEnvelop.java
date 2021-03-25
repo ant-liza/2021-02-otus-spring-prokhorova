8873db3fc6af013f706a71dd738edae18538944b
Elizaveta.Prokhorova <prelizaweta@yandex.ru>
2021-03-25 20:33:41 +0300

    Добавлна оберта для MessageSource, и в ней локаль

---
+++ homework01/src/main/java/ru/otus/homework01/bean/CustomMessageSourceEnvelop.java
@@ -0,0 +1,19 @@
+package ru.otus.homework01.bean;
+
+import org.springframework.context.MessageSource;
+
+public class CustomMessageSourceEnvelop {
+    private final MessageSource messageSource;
+    private final CustomLocaleEnvelope localeEnvelope;
+
+    public CustomMessageSourceEnvelop(MessageSource messageSource,
+                                      CustomLocaleEnvelope localeEnvelope) {
+        this.messageSource = messageSource;
+        this.localeEnvelope = localeEnvelope;
+    }
+
+    public String getMessage(String value) {
+        return messageSource.getMessage(value,
+                null, localeEnvelope.getDefaultLocale());
+    }
+}
